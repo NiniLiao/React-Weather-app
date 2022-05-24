5f3d917a38034497507b3b4d0b1416b47b0e6364
Nini Liao <u9513031@cmu.edu.tw>
2022-05-24 22:48:17 +0800

    updated Date time

--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -12,109 +12,47 @@ function DisplayWeather(props) {
         <React.Fragment>
           <div className="maincard">
             <span className="cardtitle">
-              {data.name} , {data.sys.country}. Weather
-            </span>
-            <span className="cardsubtitle">
-              As of {new Date().toLocaleTimeString()}
+              {data.name} , {data.sys.country}
             </span>
 
-            <h1>
+            <span className="cardsubtitle-text">
+              Time:
+            </span>
+            <span className="cardsubtitle">
+              {new Date().toLocaleString('en-US', { hour12: true }).replace(/\//g, '-')}
+            </span>
+
+            <span className="weather-temperature-text">  
+            Temperature:
+            </span>   
+            <span className="weather-temperature">
               {" "}
-              {Math.floor(data.main.temp - 273.15)}
-              <sup>o</sup>
-            </h1>
+               
+              {Math.floor(data.main.temp_min - 273.15)} 
+              <sup>o</sup>C
+              ~ 
+              {Math.floor(data.main.temp_max - 273.15)}
+              <sup>o</sup>C
+            </span>
             <span className="weather-main">{data.weather[0].main}</span>
             <img className="weather-icon" src={iconurl} alt="" srcset="" />
+            <span className="weather-description-text">
+              Description: 
+            </span>
             <span className="weather-description">
               {" "}
               {data.weather[0].description}
             </span>
-          </div>
-          <div className="weatherdetails">
-            <div className="section1">
-              <table>
-                <tr>
-                  <td>
-                    <h4>High/Low</h4>
-                  </td>
-                  <td>
-                    <span>
-                      {Math.floor(data.main.temp_max - 273.15)}/
-                      {Math.floor(data.main.temp_min - 273.15)}
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Humidity</h4>
-                  </td>
-                  <td>
-                    <span>{data.main.humidity} %</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Pressure</h4>
-                  </td>
-                  <td>
-                    <span>{data.main.pressure} hPa</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Visibility</h4>
-                  </td>
-                  <td>
-                    <span>{data.visibility / 1000} Km</span>
-                  </td>
-                </tr>
-              </table>
-            </div>
+            
+            <span className="weather-humidity-text">  
+              Humidity:
+            </span>   
+            <span className="weather-humidity">
+              {" "}
+              {data.main.humidity} % 
+            </span>
 
-            <div className="section2">
-              <table>
-                <tr>
-                  <td>
-                    <h4>Wind</h4>
-                  </td>
-                  <td>
-                    <span>{Math.floor((data.wind.speed * 18) / 5)} km/hr</span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Wind Direction</h4>
-                  </td>
-                  <td>
-                    <span>
-                      {data.wind.deg}
-                      <sup>o</sup> deg
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Sunrise</h4>
-                  </td>
-                  <td>
-                    <span>
-                      {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
-                    </span>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <h4>Sunset</h4>
-                  </td>
-                  <td>
-                    <span>
-                      {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
-                    </span>
-                  </td>
-                </tr>
-              </table>
-            </div>
-          </div>
+          </div>          
         </React.Fragment>
       ) : (
         <div className="maincard">
